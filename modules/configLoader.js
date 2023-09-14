c57ce7c063e817343720d11f32f29606a936d51a
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-09-14 19:30:57 +0200

    detect components in folder to config.json

--- modules/configLoader.js
+++ modules/configLoader.js
@@ -5,6 +5,21 @@ class ConfigLoader{
 
     constructor(){
         this.config = JSON.parse(fs.readFileSync("./config.json"));
+        this.componentMapper();
+    }
+
+    componentMapper(){
+        let compDir = fs.readdirSync("./src/components")
+        compDir.forEach(comp => {
+            if (comp.includes(".html")) {
+                this.config["components"].push(comp.split(".html")[0]);
+            }
+        });
+        this.overWritteConfig(JSON.stringify(this.config));
+    }
+
+    overWritteConfig(config){
+        fs.writeFileSync("./config.json", config);
     }
 
 }
