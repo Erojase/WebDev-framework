21ab69db8404ee751668526ad126a278a939518f
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-09-15 18:30:09 +0200

    beta router feature

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 A web development framework attempt
 
 - [Features](#features)
+- [Quickstart](#quickstart)
+
 
 ## Features
+---
 - [x] Local server deployment
 - [x] Configuration json file
 - [ ] Page routing
 - [ ] Live reload every time a file is modified
 - [x] HTML components
 - [ ] Typescript support
+- [ ] Quickstart page
 
+<br>
+
+## Quickstart
+---
--- modules/router.js
+++ modules/router.js
@@ -1,25 +1,49 @@
+const fs = require("fs");
 
+class Router {
+	/**
+	 * @param {string} indexPath the file path to the index file
+	 */
+	constructor(indexPath) {
+		this.routes = [new Route(indexPath, "/")];
+		this.loadRoutes();
+	}
 
-class Router{
+	loadRoutes() {
+		let contents = fs.readdirSync("./src/pages");
+		contents.forEach((elem) => {
+			if (elem.includes(".html")) {
+				this.routes.push(
+					new Route("./src/pages/" + elem, "/" + elem.split(".html")[0])
+				);
+			}
+		});
+	}
 
-
-    constructor(indexPath){
-        this.loadRoutes();
-        this.index = indexPath;
-    }
-
-    loadRoutes(){
-
-    }
-
-    route(path){
-        if (path == "/") {
-            return {"status":200, "path":this.index};
-        }
-
-        return {"status": 400, "path":this.index};
-    }
+	route(path) {
+		let pageFound = { status: 400, path: this.routes[0].path };
+		this.routes.forEach((route) => {
+			if (path == route.route) {
+				pageFound = { status: 200, path: route.path };
+			}
+		});
+		return pageFound;
+	}
 }
 
+/** Class for storing a route */
+class Route {
+	/**
+	 *
+	 * @param {string} path the path of the html file in the server from the project root
+	 * @param {string} route the web route with the / character e.g. "/profile"
+	 * @param {"static"|"other"} type type of the route, it can be a static route that goes to a existing html page or
+	 */
+	constructor(path, route, type = "static") {
+		this.path = path;
+		this.route = route;
+		this.type = type;
+	}
+}
 
 module.exports.Router = Router;
